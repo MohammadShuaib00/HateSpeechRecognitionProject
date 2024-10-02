42cd3b684fcb262c8043de531c4806262a8f376c
Mohammad Shuaib <mohammadshuaib3455@gmail.com>
2024-10-02 13:05:03 +0530

    Folder Structured Update

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # HateSpeechRecognitionProject
+
+## How to run?
+
+```bash
+conda create -n hatespeech python=3.12 -y
+```
+
+```bash
+conda activate hatespeech
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+import os, sys
+
+setup(
+
+    name="Hate-Speech-Classification",
+    version="0.0.0.1",
+    author="Mohammad Shuaib",
+    author_email="mohammadshuaib3455@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
